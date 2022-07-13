f056fc5079c152a340071f3f63e23e81dc58353b
kuanghc1 <kuanghc1@lenovo.com>
2022-07-13 17:39:59 +0800

    Add todo

--- chapter5/configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
+++ chapter5/configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// TODO: 2022/7/13 kuanghaochuan We need to add vault
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServerApplication {
--- chapter7/licensing-service/src/main/java/com/optimagrowth/license/repository/LicenseRepository.java
+++ chapter7/licensing-service/src/main/java/com/optimagrowth/license/repository/LicenseRepository.java
@@ -1,14 +1,14 @@
 package com.optimagrowth.license.repository;
 
-import java.util.List;
-
+import com.optimagrowth.license.model.License;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.optimagrowth.license.model.License;
+import java.util.List;
 
 @Repository
-public interface LicenseRepository extends CrudRepository<License,String>  {
+public interface LicenseRepository extends CrudRepository<License, String> {
     public List<License> findByOrganizationId(String organizationId);
-    public License findByOrganizationIdAndLicenseId(String organizationId,String licenseId);
+
+    public License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
 }
